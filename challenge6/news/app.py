523e867acc813d103f1cf4420911d261b56ee1fa
hades_zhao <hades_zhao@126.com>
2017-12-21 19:48:29 +0800

    index is finished

--- challenge6/news/app.py
+++ challenge6/news/app.py
@@ -8,6 +8,7 @@ def filedict_from_json(filename):
     with open(filename,'r') as file:
         filedict = json.loads(file.read())
     return filedict
+
 filename_list = os.listdir('../files')
 file_dict_list = [ filedict_from_json('../files/'+filename) \
                   for filename in filename_list ]
@@ -18,9 +19,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    title_list
-    return render_template('index.html',title=title)
+    title_list = [ file_dict.get('title') for file_dict in file_dict_list ]
+    return render_template('index.html',title_list=title_list)
 
 @app.route('/files/<filename>')
 def file(filename):
-    pass
+    _file_dict = filedict_from_json('../files/'+ filename + '.json')
+    return render_template('file.html',file_dict=_file_dict)
